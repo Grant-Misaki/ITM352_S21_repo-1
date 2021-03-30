0dac9785af33ef3948e5f9a7aeb7c574d24327ff
vkashiwai <77433309+vkashiwai@users.noreply.github.com>
2021-03-30 00:32:03 -1000

    lll

---
+++ Kashiwai_Victoria_Assignment1/product_data.js
@@ -0,0 +1,29 @@
+//Initialize product variables
+products = [
+    {
+        'name': "Cola",
+        'price': 3.35,
+        'image': "http://vkashiwai.github.io/images/Tokyo.jpeg"
+    },
+    {
+        'name': "Grape",
+        'price': 4.57,
+        'image': "http://vkashiwai.github.io/images/Kyoto.jpeg"
+    },
+    {
+        'name': "Peach",
+        'price': 3.43,
+        'image': "http://vkashiwai.github.io/images/Osaka.jpeg"
+    },
+    {
+        'name': "Plum",
+        'price': 4.19,
+        'image': "http://vkashiwai.github.io/images/Kobe.jpeg"
+    },
+    {
+        'name': "Lemon",
+        'price': 2.87,
+        'image': "http://vkashiwai.github.io/images/All.jpeg"
+    } 
+]
+

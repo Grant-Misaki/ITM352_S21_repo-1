595d229b754e7c3a12c408d2328d5cadb98f5cee
vkashiwai <77433309+vkashiwai@users.noreply.github.com>
2021-03-30 23:31:09 -1000

    lala

--- Kashiwai_Victoria_Assignment1/product_data.js
+++ Kashiwai_Victoria_Assignment1/product_data.js
@@ -3,27 +3,27 @@ products = [
     {
         'name': "Cola",
         'price': 3.35,
-        'image': "http://vkashiwai.github.io/images/Tokyo.jpeg"
+        'image': "http://vkashiwai.github.io/images/Cola.jpg"
     },
     {
         'name': "Grape",
         'price': 4.57,
-        'image': "http://vkashiwai.github.io/images/Kyoto.jpeg"
+        'image': "http://vkashiwai.github.io/images/Grape.jpg"
     },
     {
         'name': "Peach",
         'price': 3.43,
-        'image': "http://vkashiwai.github.io/images/Osaka.jpeg"
+        'image': "http://vkashiwai.github.io/images/Peach.jpg"
     },
     {
         'name': "Plum",
         'price': 4.19,
-        'image': "http://vkashiwai.github.io/images/Kobe.jpeg"
+        'image': "http://vkashiwai.github.io/images/Plum.jpg"
     },
     {
         'name': "Lemon",
         'price': 2.87,
-        'image': "http://vkashiwai.github.io/images/All.jpeg"
+        'image': "http://vkashiwai.github.io/images/Lemon.jpg"
     } 
 ]
 
---
+++ WODs/Invoice3/product_data.js
@@ -0,0 +1,26 @@
+//Initialize product variables
+var product1 = {
+    name: "HTC",
+    price: 40.00,
+    image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg"
+}
+var product2 = {
+    name: "Apple",
+    price: 75.00,
+    image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg"
+}
+var product3 = {
+    name: "Nokia",
+    price: 35.00,
+    image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg"
+}
+var product4 = {
+    name: "Samsung",
+    price: 45.00,
+    image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg"
+}
+var product5 = {
+    name: "Blackberry",
+    price: 10.00,
+    image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg"
+} 

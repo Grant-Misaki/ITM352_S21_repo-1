b4a1f1ea38362334a1d412e5deda6a17746fceeb
vkashiwai <77433309+vkashiwai@users.noreply.github.com>
2021-04-20 13:00:48 -1000

    o

---
+++ Lab14/public/product_data.js
@@ -0,0 +1,32 @@
+//Initialize product variables
+products = [
+    {
+        'name': "Cola",
+        'price': 3.35,
+        'image': "http://vkashiwai.github.io/images/Cola.jpg"
+    },
+    {
+        'name': "Grape",
+        'price': 4.57,
+        'image': "http://vkashiwai.github.io/images/Grape.jpg"
+    },
+    {
+        'name': "Peach",
+        'price': 3.43,
+        'image': "http://vkashiwai.github.io/images/Peach.jpg"
+    },
+    {
+        'name': "Plum",
+        'price': 4.19,
+        'image': "http://vkashiwai.github.io/images/Plum.jpg"
+    },
+    {
+        'name': "Lemon",
+        'price': 2.87,
+        'image': "http://vkashiwai.github.io/images/Lemon.jpg"
+    } 
+]
+if(typeof module != 'undefined') {
+    module.exports.data = products;
+}
+
